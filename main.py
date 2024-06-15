974c66aa4b25b2ce27a70f98999e1452e7267bab
Megha Vaswani <128133837+megha-vaswani2309@users.noreply.github.com>
2024-06-15 13:15:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ add_bg_from_url()
 
 
 # Load weather data
-weather_data = pd.read_csv("C:\Users\prach\Downloads\weather_analysis-main\weather_analysis-main\WeatherData.csv")
+weather_data = pd.read_csv(r"C:\Users\prach\Downloads\weather_analysis-main\weather_analysis-main\WeatherData.csv")
 
 
 
